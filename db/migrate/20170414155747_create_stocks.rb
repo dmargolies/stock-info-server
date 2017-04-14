7b3cb8db803f2effaf11d4234574b46261191536
David Margolies <dmargolie@gmail.com>
2017-04-14 11:12:32 -0500

    initial commit

---
+++ db/migrate/20170414155747_create_stocks.rb
@@ -0,0 +1,16 @@
+class CreateStocks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stocks do |t|
+      t.string :symbol
+      t.string :name
+      t.decimal :last_sale
+      t.decimal :market_cap
+      t.integer :adr_tso
+      t.integer :ipo_year
+      t.string :sector
+      t.string :industry
+      t.string :summary_quote
+      t.timestamps
+    end
+  end
+end
